87d80e04f065ac75f0ff8af342e056e63b1f64a0
Ian <ian (at) ianrobinson.net>
2018-05-25 13:43:10 +0200

    Add table and id for when the XMLHttp can input API data as well as created the XMLHttp request with data displaying temporarily in the demo position

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,15 @@
-console.log('Leaderboard.js functioning');
+window.onload = init;
+
+function init() {
+  console.log('Window has loaded, executing JS');
+  var fccCamperRequest = new XMLHttpRequest();
+
+  fccCamperRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = fccCamperRequest.responseText;
+    }
+  };
+
+  fccCamperRequest.open("GET", "https://fcctop100.herokuapp.com/api/fccusers/top/recent", true);
+  fccCamperRequest.send();
+}
